ad221ccfa07c6cce24f7f50525306e56663bb50e
Alex Pascual Gangolells <alexpascual@MacBook-Pro-de-Alex.local>
2025-01-15 10:33:20 +0100

    Actualización: Pantalla principal conectada a todas las pantallas

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'create_routine_screen.dart';
+import 'recommended_screen.dart';
+import 'progress_screen.dart';
+import 'settings_screen.dart';
 
 class MainScreen extends StatelessWidget {
   @override
@@ -7,10 +11,53 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Pantalla Principal'),
       ),
-      body: Center(
-        child: Text(
-          '¡Bienvenido a la Pantalla Principal!',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CreateRoutineScreen()),
+                );
+              },
+              child: Text('Crear Rutina Personalizada'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => RecommendedScreen()),
+                );
+              },
+              child: Text('Ver Rutinas Recomendadas'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProgressScreen()),
+                );
+              },
+              child: Text('Ver Seguimiento del Progreso'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SettingsScreen()),
+                );
+              },
+              child: Text('Configuración'),
+            ),
+          ],
         ),
       ),
     );
